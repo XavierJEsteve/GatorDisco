37a62e20861da76d6c1830d87384abc8cb990713
erives <43967113+erives@users.noreply.github.com>
2022-01-13 12:04:12 -0500

    continued Testing on PeakEQ
    tried to add a flag which would only update coeffiecients upon parameter change but that ended up not working as it wouldn't affect the audio at all.

--- Evan's LaunchPad/PeakEq.c
+++ Evan's LaunchPad/PeakEq.c
@@ -42,8 +42,8 @@ float32 A = 0.0;    //this will control the gain (I think)
 float32 w0 = 0.0;
 float32 cosParam = 0.0; //cos(w0)
 float32 sinParam = 0.0; //sin(w0)
-float32 Q = 0.707;    //vary between 0.1 and 10. Bigger Q == narrow width
-float32 fCenter = 2000.0; //(keep between 10 and 1000 Hz)
+float32 Q = 1.414214;    //Q will be static, this covers one octave
+float32 fCenter = 5100.0; //(keep between 10 and 1000 Hz)
 Uint16 Fs = 48000;
 
 int16 sampleIn = 0;
@@ -58,6 +58,7 @@ int16 xd1,xd2,yd1,yd2 = 0;
 //adc data
 Uint16 adcDataCh0, adcDataCh1,adcDataCh2 = 0;
 float32 cFreqRatio, gainRatio, qRatio = 0.0;
+Uint16 ParamUpdate = 0;
 
 
 int main(void)
@@ -114,16 +115,48 @@ int main(void)
     while(1)
     {
 
+/*        if(ParamUpdate == 1)
+        {
+            //get ratio of each channel to control biquad parameters
+            cFreqRatio = (adcDataCh0 / 4095.0);
+            gainRatio = (adcDataCh1/ 4095.0);
+            //qRatio = (adcDataCh2/ 4095.0);
+
+            //get range of parameters and multiply by ratio to get value to add to low end
+            // i.e : 1000Hz - 10Hz = 990 Hz  ==> 990Hz * (0.5 ratio) = 495 ==> 495Hz + 10Hz = 505Hz
+            fCenter = ((1800 * cFreqRatio) + 200);
+            dbGain  = ((30 * gainRatio) + -15);
+            //Q       = ((9.9 * qRatio) + 0.1);
+
+            //these values along with the coefficients will be placed in a struct ideally (possibly previous samples as well)
+            //re-calculate variables for filter coeffiecients
+            A = powf(10, dbGain/40.0);           //controls gain
+            w0 = PI2 * (float32)(fCenter/Fs);   //controls center frequency
+            cosParam = cosf(w0);
+            sinParam = sinf(w0);
+            alpha = (sinParam/(2*Q));           //controls Q
+
+            //might do small size 2 arrays to keep track of previous outputs????
+            b0 = 1 + alpha*A; //b0 = 1 + a*A
+            b1 = -2*cosParam; //b1 = -2cos(w0)
+            b2 = 1 - alpha*A; //b2 = 1 - a*A
+            a0 = 1 + (alpha/A); //a0 = 1+(alpha/A)
+            a1 = b1;           //a1 same as b1, -2cos(w0)
+            a2 = 1 - (alpha/A); //a2 = 1-(alpha/A)
+
+            ParamUpdate = 0;
+        }*/
+
         //get ratio of each channel to control biquad parameters
         cFreqRatio = (adcDataCh0 / 4095.0);
         gainRatio = (adcDataCh1/ 4095.0);
-        qRatio = (adcDataCh2/ 4095.0);
+        //qRatio = (adcDataCh2/ 4095.0);
 
         //get range of parameters and multiply by ratio to get value to add to low end
         // i.e : 1000Hz - 10Hz = 990 Hz  ==> 990Hz * (0.5 ratio) = 495 ==> 495Hz + 10Hz = 505Hz
-        fCenter = ((990 * cFreqRatio) + 10);
+        fCenter = ((9800 * cFreqRatio) + 200);
         dbGain  = ((30 * gainRatio) + -15);
-        Q       = ((9.9 * qRatio) + 0.1);
+        //Q       = ((9.9 * qRatio) + 0.1);
 
         //these values along with the coefficients will be placed in a struct ideally (possibly previous samples as well)
         //re-calculate variables for filter coeffiecients
@@ -141,10 +174,10 @@ int main(void)
         a1 = b1;           //a1 same as b1, -2cos(w0)
         a2 = 1 - (alpha/A); //a2 = 1-(alpha/A)
 
+        //DIRECT FORM 1 DIFFERENCE EQUATION
         //y[n]= (b0/a0)*x[n] + (b1/a0)x[n−1] + (b2/a0)x[n−2] − (a1/a0)y[n−1] − (a2/a0)y[n−2]
 
         sampleOut = (b0/a0)*sampleIn + (b1/a0)*xd1 + (b2/a0)*xd2 - (a1/a0)*yd1 - (a2/a0)*yd2;
-        //sampleOut = (b0)*sampleIn + (b1)*xd1 + (b2)*xd2 - (a1)*yd1 - (a2)*yd2;
 
         //shift the samples
         xd2 = xd1;
@@ -172,9 +205,11 @@ void InitAdca(void) {
     AdcaRegs.ADCSOC1CTL.bit.ACQPS = 14;                                // Sets sample and hold window -> must be at least 1 ADC clock long
     AdcaRegs.ADCSOC1CTL.bit.TRIGSEL= 2;                                // Set the adc to trigger on Timer1 interrupt
 
+/*
     AdcaRegs.ADCSOC2CTL.bit.CHSEL = 2;                                 // Sets SOC0 to channel 0 -> pin ADCINA0
     AdcaRegs.ADCSOC2CTL.bit.ACQPS = 14;                                // Sets sample and hold window -> must be at least 1 ADC clock long
     AdcaRegs.ADCSOC2CTL.bit.TRIGSEL= 2;                                // Set the adc to trigger on Timer1 interrupt
+*/
 }
 
 void InitTimer1(void) {
@@ -204,9 +239,10 @@ interrupt void Timer1_isr(void) {
 
     //AdcaRegs.ADCSOCFRC1.all = 0x1;          //0x07 == Force conversion on channel 0,1,2 | 0x03 == conversion on channel 0,1
 
+    //ParamUpdate = 1;
     adcDataCh0 = AdcaResultRegs.ADCRESULT0;    // Read ADC result into global variable
     adcDataCh1 = AdcaResultRegs.ADCRESULT1;
-    adcDataCh2 = AdcaResultRegs.ADCRESULT2;
+    //adcDataCh2 = AdcaResultRegs.ADCRESULT2;
 }
 
 void GPIO_INIT()
