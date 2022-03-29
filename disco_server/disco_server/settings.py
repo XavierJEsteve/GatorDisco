87a0dda49ae5f161ce36200924cb2ac3951f6a5e
xavierjesteve <xesteve@ufl.edu>
2022-03-29 11:52:58 -0400

    post merge cleanup

--- disco_server/disco_server/settings.py
+++ disco_server/disco_server/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'crispy_forms',
     'fileshare',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -139,4 +138,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-CRISPY_TEMPLATE_PACK = 'uni_form'
