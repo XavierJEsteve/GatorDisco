74deeccf9142e7c36fff08f6e8c95a8b0da709e5
XavierJEsteve <xesteve@ufl.edu>
2022-04-04 14:56:11 -0400

    Think I restored the removed RPi directory.
    Former-commit-id: 0fb7ae2e0caa6f4932ecc23fb572e1c29b337988

---
+++ disco_render/RPi/spi_test.c
@@ -0,0 +1,31 @@
+
+
+#include <errno.h>
+#include <wiringPiSPI.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+// channel is the wiringPi name for the chip select (or chip enable) pin.
+// Set this to 0 or 1, depending on how it's connected.
+static const int CHANNEL = 0;
+
+int main()
+{
+   int fd, result;
+   unsigned char buffer[100];
+
+   // CHANNEL insicates chip select,
+   // 500000 indicates bus speed.
+   fd = wiringPiSPISetup(CHANNEL, 500000);
+
+   int spiData = 0;
+   while(1)
+   {
+   	buffer[0] = spiData;
+      spiData++;
+      buffer[1] = spiData;
+      spiData++;
+      result = wiringPiSPIDataRW(CHANNEL, buffer, 2);
+   }
+}
