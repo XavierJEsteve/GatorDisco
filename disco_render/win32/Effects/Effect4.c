8e39018f255a05781aab905322887aa420411e29
Dylan VanStaden <dvanstaden1@ufl.edu>
2022-04-06 22:47:18 -0400

    Phaser effects

--- disco_render/win32/Effects/Effect4.c
+++ disco_render/win32/Effects/Effect4.c
@@ -3,18 +3,61 @@
 */
 
 #include "Effect4.h"
+#include <stdio.h>
 
-static float effect4param1;
-static float effect4param2;
+static float phaseBuffer[4096];
+static int PHASEBUFFERSIZE_MASK = 4095;
+static unsigned int p = 4095;          //default max phase
+static int feedback = 0 ; //default half phase volume
 
 
-void updateEffect4Params(float param1, float param2)
+void updateEffect4Params(float time, int feedbackLine)
 {
-    effect4param1 = param1;
-    effect4param2 = param2;
+    //update phase shift with time
+    if(time > 0.085)
+        p  = (48000 * 0.085);
+    else
+        p  = (48000 * time);
+
+    if(0 < feedbackLine && feedbackLine < 5)
+        feedback = feedbackLine;
 }
+
+/*
+float cascade(float liveData, float buffData, float shift, int fback, int head, int tail, int myPointer)
+{
+    int n = fback;
+    if (n > 0)
+    {
+        return cascade(liveData, buffData, shift, n-1);
+    }
+    else
+    {
+        return liveData/fback + buffData/fback;
+    }
+}
+*/
 
 float processEffect4(float sampleIn)
 {
-    return sampleIn;
+    float computedPhase;
+    float bufferdata;
+
+    static int phaseHead = 0;
+    static int phaseTail = 0;
+    static int phasePointer = 0;
+
+    phasePointer = (phaseTail - p) & PHASEBUFFERSIZE_MASK;
+
+    bufferdata = phaseBuffer[phasePointer];
+    
+    computedPhase = bufferdata;
+    //computedPhase = cascade(sampleIn, bufferdata, p, feedback, phaseHead, phaseTail, phasePointer);
+    
+    phaseBuffer[phaseHead] = computedPhase;          //send to buffer
+
+    phaseHead = ((phaseHead + 1) & PHASEBUFFERSIZE_MASK);       //increment head head= head + 2 & BUFFERSIZE(0x7FFFF)
+    phaseTail = (phaseTail + 1) & PHASEBUFFERSIZE_MASK;         // increment tail pointer
+
+    return computedPhase;
 }
